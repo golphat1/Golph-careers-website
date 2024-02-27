92ab73e5d48e6c257a4a6808504d90eacfe4b285
golphat1 <118168814+golphat1@users.noreply.github.com>
2024-02-27 19:14:54 +0300

    Update app.py

--- careers/app.py
+++ careers/app.py
@@ -8,18 +8,18 @@ JOBS = [
             'title': 'Data Scientist',
             'location': 'Nairobi, Kenya',
             'salary':'ksh. 350,000'
-            }
+            },
         {
             'id': 2,
             'title': 'Data Analyst',
             'location': 'Kampala, Uganda',
             'salary':'ush. 5,000,000'
-            }
+            },
         {
             'id': 3,
             'title': 'Frontend Engineer',
             'location': 'Califonia, USA'
-            }
+            },
         {
             'id': 4,
             'title': 'Backend Engineer',
@@ -39,4 +39,4 @@ def list_jobs():
     return jsonify(JOBS)
 
 if __name__== "__main__":
-    app.run(host=0.0.0.0, debug=True)
+    app.run(host='0.0.0.0', debug=True)
